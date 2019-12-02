6c4c9eb9f3f1d4f260b8ed91211ae4dab32ef12a
Timo Aho <zeukkari@gmail.com>
2019-12-02 12:13:43 +0200

    linting

--- apollo.config.js
+++ apollo.config.js
@@ -3,9 +3,9 @@ require('dotenv').config({ path: '.env.development.local' });
 
 module.exports = {
   client: {
+    excludes: ['**/__tests__/**'],
+    includes: ['./src/apollo/resolvers.ts", "./schema.json'],
     service: 'tutorial-zeukkari',
     url: process.env.REACT_APP_API_URI,
-    excludes: ["**/__tests__/**"],
-    includes: ["./src/apollo/resolvers.ts", "./schema.json"]
   },
 };
