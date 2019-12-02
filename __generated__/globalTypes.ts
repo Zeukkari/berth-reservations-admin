d550b6341442e94d9aac07a5dddab07bcae864c6
Timo Aho <zeukkari@gmail.com>
2019-12-02 12:16:27 +0200

    add graphql enum types

---
+++ __generated__/globalTypes.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum AddressAddressType {
+  A_ = "A_",
+  HOME = "HOME",
+  OTHER = "OTHER",
+  WORK = "WORK",
+}
+
+export enum ContactMethod {
+  EMAIL = "EMAIL",
+  SMS = "SMS",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
