d8ee3aec1433e62e2c5ffe6bd1148ecb01219aea
Timo Aho <zeukkari@gmail.com>
2020-01-28 13:00:55 +0200

    use Helsinki Grotesk font family

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import './assets/styles/main.scss';
+// import './assets/styles/main.scss';
 import './locales/i18n';
+import './font.css';
 import 'hds-core/lib/helsinki.css';
 
 import * as Sentry from '@sentry/browser';
