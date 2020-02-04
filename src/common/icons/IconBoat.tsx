972e4753f03a8fd65e362528a9b1e27929fa121c
Timo Aho <zeukkari@gmail.com>
2020-02-04 08:45:26 +0200

    normalize svg viewboxes

--- src/common/icons/IconBoat.tsx
+++ src/common/icons/IconBoat.tsx
@@ -7,7 +7,7 @@ export default ({ className = '' }: Props) => (
     xmlns="http://www.w3.org/2000/svg"
     width="100%"
     height="100%"
-    viewBox="0 0 199.404 199.404"
+    viewBox="0 0 24 24"
     className={className}
   >
     <g id="prefix__prefix__Symbols">
